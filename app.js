c93559d20b6aab7988210fbf51821cc55457e4a0
gabrieltorres519 <gabrieltorres9909@gmail.com>
2022-05-04 12:42:54 -0500

    Se agregó la configuración necesaria para crear la card, usando un archivo .env

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+require('dotenv').config(); // Se requiere a la configuración del archivo .env en 
+                            // donde definiremos los datos de autenticación 
+
+if(!process.env.TOKEN && !process.env.KEY){
+    throw new Error('No hay configuración con Api Key y con Token');
+}
+
 const Trello = require("./main");
-var trello = new Trello("key", "token");
-var cardTitle = `Card Nueva ${new Date()}`;
+var trello = new Trello(process.env.KEY, process.env.TOKEN);
+var cardTitle = `Card Nueva Gabriel con forked project ${new Date()}`;
 
 console.log("Ejecutando!");
 
-trello.addCard(cardTitle, "LaunchX Card Description", "IdList",
+trello.addCard(cardTitle, "LaunchX Card Description", "6272b12a422ab2203fbbd175",
   function (error, trelloCard) {
     if (error) {
       console.log('Could not add card:', error);
